04b1dd45581299ec5ee237c02d071b784baefa6e
radensaleh <m.salehidris13@gmail.com>
2018-12-21 13:26:52 +0700

    https fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,22 +11,22 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot(UrlGenerator $url)
-    {
-        if(env('REDIRECT_HTTPS')){
-          $url->formatScheme('https');
-        }
-    }
+     public function boot(UrlGenerator $url)
+     {
+         if(env('REDIRECT_HTTPS')) {
+             $url->formatScheme('https');
+         }
+     }
 
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-      if(env('REDIRECT_HTTPS')){
-        $this->app['request']->server->set('HTTPS', true);
-      }
-    }
+   /**
+    * Register any application services.
+    *
+    * @return void
+    */
+     public function register()
+     {
+         if(env('REDIRECT_HTTPS')) {
+             $this->app['request']->server->set('HTTPS', true);
+         }
+     }
 }
